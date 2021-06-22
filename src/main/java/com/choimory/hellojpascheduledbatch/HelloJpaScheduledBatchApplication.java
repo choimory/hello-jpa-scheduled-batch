bbffe022d4396a2bb64fff280784ac5e54419415
choimory <choimory@naver.com>
2021-06-22 21:56:29 +0900

    스케쥴링 제외 Member->Member2 기본 배치 완성

--- src/main/java/com/choimory/hellojpascheduledbatch/HelloJpaScheduledBatchApplication.java
+++ src/main/java/com/choimory/hellojpascheduledbatch/HelloJpaScheduledBatchApplication.java
@@ -1,9 +1,11 @@
 package com.choimory.hellojpascheduledbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class HelloJpaScheduledBatchApplication {
 
     public static void main(String[] args) {
