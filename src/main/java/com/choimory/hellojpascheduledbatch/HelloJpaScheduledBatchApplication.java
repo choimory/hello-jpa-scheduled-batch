3757d8faa4f969c12c1ea23a1f85a2624807fe9d
choimory <choimory@naver.com>
2021-06-22 21:33:09 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hellojpascheduledbatch/HelloJpaScheduledBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hellojpascheduledbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpaScheduledBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJpaScheduledBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/hellojpascheduledbatch/HelloJpaScheduledBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.hellojpascheduledbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaScheduledBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
